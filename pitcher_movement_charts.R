65f0e676d94dcc3f2697c8705fe6694d9b222d2c
Ajay Patel <89551683+ajaypatell8@users.noreply.github.com>
2023-04-04 11:48:14 -0400

    Update pitcher_movement_charts.R

--- pitcher_movement_charts.R
+++ pitcher_movement_charts.R
@@ -2,15 +2,22 @@ library(tidyverse)
 library(ggplot2)
 library(baseballr)
 library(readr)
+library(data.table)
 
-savantData <- read_csv("SavantData.csv")
+savantData <- fread("/Users/ajaypatel8/Downloads/SavantData.csv")
 
 mlbplayerids <- baseballr::chadwick_player_lu()
 
 strip_id <- mlbplayerids %>% 
-  filter(name_first == "Pierce", name_last == "Johnson")
+  filter(name_first == "Hayden", name_last == "Wesneski")
 
 strip_id <- as.vector(strip_id)
+
+savantData %>% 
+  filter(game_year == 2022) %>% 
+  View()
+
+unique(savantData$game_year)
 
 pitcher_chart <- savantData %>% 
   filter(pitcher == strip_id$key_mlbam) %>% 
@@ -18,15 +25,16 @@ pitcher_chart <- savantData %>%
   mutate(pfx_z = pfx_z * 12, pfx_x = pfx_x *12) %>% 
   select(player_name, pitcher, plate_x, plate_z, pfx_x, pfx_z, pitch_type, pitch_name, game_year) %>%
   ggplot(aes(x = pfx_x, y = pfx_z, group = pitch_name)) +
-  facet_wrap(~game_year) +
-  stat_ellipse(aes(x = pfx_x, y = pfx_z, fill = pitch_name), geom = "polygon", level = 0.6) +
+  #facet_wrap(~game_year) +
+  geom_point(aes(color = pitch_name), alpha = 0.4) +
+  stat_ellipse(aes(x = pfx_x, y = pfx_z, color = pitch_name), level = 0.6) +
   theme_ajay() +
   ggtitle(paste(strip_id$name_first," ", strip_id$name_last, "'s Movement Chart", sep = '')) +
   theme(
     legend.position = "top",
     legend.title = element_blank()
   ) +
-  labs(x = "Horizontal Movement",
+  labs(x = "Horizontal Movement (Catcher's Perspective)",
        y = "Vertical Movement",
        caption = 'Data: BaseballSavant | @ajaypatell8') +
   ylim(-20, 20) +
