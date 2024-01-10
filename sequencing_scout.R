640584620d03dd085c1720a84ec9398ef32a7ffb
Ajay Patel <89551683+ajaypatell8@users.noreply.github.com>
2024-01-10 17:18:46 -0500

    improve runtime of code

--- sequencing_scout.R
+++ sequencing_scout.R
@@ -2,6 +2,7 @@ library(data.table)
 library(tidyverse)
 library(baseballr)
 
+#read in data
 data <- fread("/Users/ajaypatel/Downloads/SavantData.csv")
 
 #2023 only
@@ -15,22 +16,17 @@ data <- data %>%
     !is.na(woba_value) & is.na(estimated_woba_using_speedangle) ~ woba_value,
     !is.na(estimated_woba_using_speedangle) ~ estimated_woba_using_speedangle,
     TRUE ~ NA
-  ))
+  ),
+  batting_team = if_else(inning_topbot == "Top", away_team, home_team),
+  pitching_team = if_else(inning_topbot == 'Top', home_team, away_team))
 
-#get batter xwOBA against first three pitches
-batter_seq <- data %>% 
-  group_by(game_date, batter_name, batter, at_bat_number) %>% 
-  slice(1:3) %>% 
-  mutate(pitches = paste(pitch_type, collapse = ','),
-         xwOBA = first(player_results)) %>% 
-  ungroup() %>% 
-  select(-at_bat_number, -pitch_type, -player_results) %>% 
-  unique() %>% 
-  group_by(batter_name, batter, pitches) %>% 
-  summarise(times_seen = n(), xwOBA = mean(xwOBA))
+#define pitching team and hitting team
+pitchingTeam <- "HOU"
+team <- "NYY"
 
 #get pitcher xwOBA against first three pitches
 pitcher_seq <- data %>% 
+  filter(pitching_team == pitchingTeam) %>% 
   group_by(game_date, pitcher_name, pitcher, at_bat_number) %>% 
   slice(1:3) %>% 
   mutate(pitches = paste(pitch_type, collapse = ','),
@@ -41,7 +37,18 @@ pitcher_seq <- data %>%
   group_by(pitcher_name, pitcher, pitches) %>% 
   summarise(times_thrown = n(), xwOBA = mean(xwOBA))
 
-rm(data)
+#get batter xwOBA against first three pitches
+batter_seq <- data %>% 
+  filter(batting_team == team) %>% 
+  group_by(game_date, batter_name, batter, at_bat_number) %>% 
+  slice(1:3) %>% 
+  mutate(pitches = paste(pitch_type, collapse = ','),
+         xwOBA = first(player_results)) %>% 
+  ungroup() %>% 
+  select(-at_bat_number, -pitch_type, -player_results) %>% 
+  unique() %>% 
+  group_by(batter_name, batter, pitches) %>% 
+  summarise(times_seen = n(), xwOBA = mean(xwOBA))
 
 library(gt)
 library(gtExtras)
@@ -52,7 +59,7 @@ library(bstfun)
 pitcherName <- "Justin Verlander"
 
 game_pitcher_seq <- pitcher_seq %>% 
-  filter(pitcher_name == pitcherName, times_thrown >= 30) %>% 
+  filter(pitcher_name == pitcherName, times_thrown >= 20) %>% 
   arrange(-desc(xwOBA)) %>% 
   filter(row_number() <= 3 | row_number() >= n() -2)
 
@@ -68,7 +75,13 @@ pitcher_seq_table <- game_pitcher_seq %>%
   ) %>% 
   gt_fmt_mlb_dot_headshot(columns = "pitcher") %>% 
   fmt_number(columns = xwOBA, decimals = 3) %>% 
-  gt_hulk_col_numeric(columns = xwOBA, reverse = TRUE) %>% 
+  data_color(
+    columns = c(xwOBA),
+    colors = scales::col_numeric(
+      palette = c(
+        "blue", "white", "red"),
+      domain = c(min(game_pitcher_seq$xwOBA), max(game_pitcher_seq$xwOBA)))
+  )  %>% 
   tab_header(
     title = paste(pitcherName, "'s Three Best and Worst Sequences", sep = ''),
     subtitle = "2023 Only | Min 30 Times Thrown | @ajaypatel8_"
@@ -79,7 +92,7 @@ pitcher_seq_table <- game_pitcher_seq %>%
   ) %>% 
   as_ggplot()
 
-#make lineup's seq table
+#make lineup's seq table------------------------------------------------------------------------------
 lineup_1 <- c("DJ LeMahieu", "Aaron Judge", "Juan Soto")
             
 lineup_2 <- c("Anthony Rizzo", "Gleyber Torres", "Alex Verdugo") 
@@ -89,6 +102,7 @@ lineup_3 <- c("Giancarlo Stanton", "Anthony Volpe", "Jose Trevino")
 lineup_1 <- factor(lineup_1)
 lineup_2 <- factor(lineup_2)
 lineup_3 <- factor(lineup_3)
+
 
 #pitcher sequences -> highlight these in tables
 pitcher_sequences <- unique(game_pitcher_seq$pitches)
@@ -100,7 +114,7 @@ top_three <- batter_seq %>%
   arrange(desc(xwOBA)) %>% 
   filter(row_number() <= 3 | row_number() >= n() -2) %>% 
   #order according to lineup
-  mutate(batter_name = factor(batter_name, levels = lineup)) %>% 
+  mutate(batter_name = factor(batter_name, levels = lineup_1)) %>% 
   arrange(batter_name)
 
 #get middle three stats
@@ -110,7 +124,7 @@ middle_three <- batter_seq %>%
   arrange(desc(xwOBA)) %>% 
   filter(row_number() <= 3 | row_number() >= n() -2) %>% 
   #order according to lineup
-  mutate(batter_name = factor(batter_name, levels = lineup)) %>% 
+  mutate(batter_name = factor(batter_name, levels = lineup_2)) %>% 
   arrange(batter_name)
 
 #get bottom three stats
@@ -120,7 +134,7 @@ bottom_three <- batter_seq %>%
   arrange(desc(xwOBA)) %>% 
   filter(row_number() <= 3 | row_number() >= n() -2) %>% 
   #order according to lineup
-  mutate(batter_name = factor(batter_name, levels = lineup)) %>% 
+  mutate(batter_name = factor(batter_name, levels = lineup_3)) %>% 
   arrange(batter_name)
 
 #make the three tables
@@ -209,12 +223,8 @@ batter_seq_table3 <- bottom_three %>%
 
 #gtsave(batter_seq_table, "/Users/ajaypatel/Downloads/RFiles/baseball/batter_seq.png")
 
-
 #use patchwork to combine tables
 library(patchwork)
-
-#define lineup team
-team <- "NYY"
 
 #final plot
 final <- pitcher_seq_table / (batter_seq_table1 + batter_seq_table2 + batter_seq_table3) +
@@ -228,8 +238,8 @@ final <- pitcher_seq_table / (batter_seq_table1 + batter_seq_table2 + batter_seq
                   plot.caption = element_mlb_logo())
   )
 
-ggsave("/Users/ajaypatel/Downloads/RFiles/baseball/seq_scout.png", final,
-       dpi = 'retina', width = 8, height = 8)
+ggsave("/Users/ajaypatel/Downloads/RFiles/baseball/sequencing/seq_scout.png", final,
+       dpi = 'retina', width = 5, height = 8)
 
 
   
